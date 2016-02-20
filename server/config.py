e39546287363432ab00146f3b90bc5e473c58f0d
山口将平 <shouhei.yamaguchi.be@gmail.com>
2016-02-20 16:19:47 +0900

    add config file

---
+++ server/config.py
@@ -0,0 +1,16 @@
+class BaseConfig:
+    _websocket_protocol = "wc://"
+    _http_protocol = "http://"
+    _endpoint = ""
+    _port = ":8888"
+
+    @classmethod
+    def http_endpoint(cls):
+        return cls._http_protocol + cls._endpoint + cls._port
+
+    @classmethod
+    def websocket_endpoint(cls):
+        return cls._websocket_protocol + cls._endpoint + cls._port
+
+class DevelopConfig(BaseConfig):
+    _endpoint = "localhost"
