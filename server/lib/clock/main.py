40d659dec761f99b719bd3f4fb9290d8e9bd71e7
山口将平 <shouhei.yamaguchi.be@gmail.com>
2016-02-20 16:11:07 +0900

    finish clock

--- server/lib/clock/main.py
+++ server/lib/clock/main.py
@@ -4,15 +4,15 @@ from websocket import create_connection
 
 if __name__ == "__main__":
     now = time.localtime()
-    for key, row in TimeTable.list():
+    for key, row in enumerate(TimeTable.table()):
         if row.is_near_start(now):
-            message = str(key) + "限がもうすぐ始まります"
+            message = str(key + 1) + "限がもうすぐ始まります"
         elif row.is_near_end(now):
-            message = str(key) + "限がもうすぐ始まります"
+            message = str(key + 1) + "限がもうすぐ終わります"
         else:
             message = "nothing"
     if message :
-        ws = create_connection("ws://localhost:8080/websocket")
+        ws = create_connection("ws://localhost:8888/websocket")
         ws.send(message)
         ws.recv()
         ws.close()
