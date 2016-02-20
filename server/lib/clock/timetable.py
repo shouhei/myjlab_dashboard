794ea3e93896184b454eadaf419232097777fe20
山口将平 <shouhei.yamaguchi.be@gmail.com>
2016-02-20 17:00:38 +0900

    fix timetable logic

--- server/lib/clock/timetable.py
+++ server/lib/clock/timetable.py
@@ -14,14 +14,14 @@ class TimeTableCell:
         return self.__end
 
     def is_near_start(self, now):
-        diff_hour = self.__start.tm_hour - now.tm_hour
-        diff_min = (diff_hour * 60) - (self.__start.tm_min - now.tm_min)
+        diff_min = (self.__start.tm_hour * 60 + self.__start.tm_min) - (now.tm_hour * 60 + now.tm_min)
+        print("start",diff_min)
         return 0 < diff_min < 5
 
     def is_near_end(self, now):
-        diff_hour = self.__end.tm_hour - now.tm_hour
-        diff_min = (diff_hour * 60) - (self.__end.tm_min - now.tm_min)
-        return -5 < diff_min < 0
+        diff_min = (self.__end.tm_hour * 60 + self.__end.tm_min) - (now.tm_hour * 60 + now.tm_min)
+        print("end:",diff_min)
+        return 0 < diff_min < 5
 
 
 class TimeTable:
