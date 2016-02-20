a17dd5bb13171e2b970d92eccf9aa7afb181b13c
山口将平 <shouhei.yamaguchi.be@gmail.com>
2016-02-20 14:23:19 +0900

    add websocket sample

---
+++ server/app.py
@@ -0,0 +1,48 @@
+import tornado.ioloop
+import tornado.web
+import tornado.websocket
+
+cl = []
+
+class WebSocketHandler(tornado.websocket.WebSocketHandler):
+    def open(self):
+        if self not in cl:
+            cl.append(self)
+
+    def on_message(self, message):
+        for client in cl:
+            client.write_message(message)
+
+    def on_close(self):
+        if self in cl:
+            cl.remove(self)
+
+
+class OtherWebSocketHandler(tornado.websocket.WebSocketHandler):
+    def open(self):
+        if self not in cl:
+            cl.append(self)
+
+    def on_message(self, message):
+        for client in cl:
+            client.write_message(message)
+
+    def on_close(self):
+        if self in cl:
+            cl.remove(self)
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("index.html")
+
+def make_app():
+    return tornado.web.Application([
+        (r"/", MainHandler),
+        (r"/websocket", WebSocketHandler),
+        (r"/other_websocket", OtherWebSocketHandler)
+    ])
+
+if __name__ == "__main__":
+    app = make_app()
+    app.listen(8888)
+    tornado.ioloop.IOLoop.current().start()
