55c9e5f241912e355edd099f2168d2d2f497c995
okunoryo <okuno.ryo.411@gmail.com>
2016-02-21 14:51:42 +0900

    2nd

---
+++ server/pi/professor.py
@@ -0,0 +1,23 @@
+# coding: utf-8
+
+import serial
+import time, sys
+from websocket import create_connection
+
+ser = serial.Serial("/dev/cu.usbmodem1411",9600)
+
+def post_professor_status(status):
+    ws = create_connection("ws://localhost:8888/websocket")
+    json_data = {'label':"professor", 'status':status, "time": time.strftime("%m月%M日%H時%M分")}
+    print("post websocket")
+    print(ws.send(str(json_data)))
+    print(ws.recv())
+    ws.close()
+
+if __name__ =="__main__":
+    print("reading...")
+    while True:
+        read_ser = ser.readline()
+        status = read_ser.decode('utf-8')[:-2]
+        post_professor_status(status)
+#        time.sleep(10)
