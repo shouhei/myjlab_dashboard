a3ecc86702353a21b45ffdb526304ad63c1301d3
山口将平 <shouhei.yamaguchi.be@gmail.com>
2016-02-21 15:47:04 +0900

    add check tool

--- server/lib/check/main.py
+++ server/lib/check/main.py
@@ -1,12 +1,13 @@
 import sys
 import os
 from websocket import create_connection
+import json
 
 if __name__ == "__main__":
     sys.path.append(os.getcwd())
     from config import ProductionConfig as Config
     message = "check"
     ws = create_connection(Config.websocket_endpoint()+"/websocket")
-    ws.send(message)
+    ws.send(json.dumps({"label":"check", "message": message}))
     ws.recv()
     ws.close()
